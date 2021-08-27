970140757011c4584d24e3b0f157b9cc018f874a
Javier Soto <javis2706@gmail.com>
2021-08-27 18:51:10 -0300

    Version Inicial

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -1,5 +1,14 @@
 package figuras;
 
-public class Rectangulo {
+
+
+public class Rectangulo(double ancho, double alto) {
+	this.ancho = ancho;
+	this.alto = alto;
+	
+	@Override
+	public String toString() {
+		return "Rectangulo [ancho=" + ancho + " , alto="¨+ alto + "]";
+	}
 
 }

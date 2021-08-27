2e2458f0e297bb12879adbb2b2c5bfe26ea8f6e8
Javier Soto <javis2706@gmail.com>
2021-08-27 19:17:23 -0300

    se agrega constructor y atributos

--- src/figuras/Rectangulo.java
+++ src/figuras/Rectangulo.java
@@ -1,14 +1,18 @@
 package figuras;
 
+public class Rectangulo {
 
+	private double ancho;
+	private double alto;
 
-public class Rectangulo(double ancho, double alto) {
-	this.ancho = ancho;
-	this.alto = alto;
-	
-	@Override
-	public String toString() {
-		return "Rectangulo [ancho=" + ancho + " , alto="¨+ alto + "]";
+	public Rectangulo(double ancho, double alto) {
+		this.ancho = ancho;
+		this.alto = alto;
 	}
+
+//	@Override
+//	public String toString() {
+//		return "Rectangulo [ancho=" + ancho + " , alto="¨+ alto + "]";
+//	}
 
 }
